5edbb3d076be6a022d94a2fc1645e301c888965a
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-07 15:53:04 -0500

    added prompt and base function

---
+++ two_city_scheduling.js
@@ -0,0 +1,19 @@
+// takes in a cost array of arrays [[]]
+// each array has a pair of numbers
+// the first number is the cost of flying 1st person to city a
+// the second number is the cost of flying 1st person to city b
+// return the minimum cost to fly every person to a city so that every person arrives in a city
+// ex. Input: costs = [[10,20],[30,200],[400,50],[30,20]]
+// Output: 110
+// Explanation: 
+// The first person goes to city A for a cost of 10.
+// The second person goes to city A for a cost of 30.
+// The third person goes to city B for a cost of 50.
+// The fourth person goes to city B for a cost of 20.
+
+// The total minimum cost is 10 + 30 + 50 + 20 = 110 to have half the people interviewing in each city.
+
+
+function twoCityScheduling(){
+
+}
