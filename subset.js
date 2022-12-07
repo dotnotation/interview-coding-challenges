369339169ebb080a584d2e6eb2efc1fd0e86f3e5
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-07 15:40:37 -0500

    added first solution, but want to find a better solution

--- subset.js
+++ subset.js
@@ -2,7 +2,20 @@
 // numbers are unique
 // return all possible sets of number combinations
 // no duplicate sets
+// returns an array of arrays
+// ex. Input: nums = [1,2,3]
+// Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
 
 function subsets(nums){
-    
+    // variable to store results
+    // maybe use recursion?
+    const results = [[]]
+    //iterate through array
+    // iterate first number against all other numbers and then remove that number
+    for (let i = 0; i < nums.length; i++){
+        for (let j=0; j < results.length; j++){
+            results.push([...results[j], nums[i]])
+        }
+    }
+    return results
 }
