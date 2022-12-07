93a224ba65996fc576279ff8850424d9eeafb1b6
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-07 13:33:18 -0500

    added notes and base function

---
+++ remove_duplicates.js
@@ -0,0 +1,15 @@
+// accepts a string(s) and an integer(k)
+// k is the number of adjacent duplicate letters
+// remove the duplicates
+// the rest of the string will be concat together
+// keep removing duplicates until there is a unique answer
+// Input: s = "deeedbbcccbdaa", k = 3
+// Output: "aa"
+// Explanation: 
+// First delete "eee" and "ccc", get "ddbbbdaa"
+// Then delete "bbb", get "dddaa"
+// Finally delete "ddd", get "aa"
+
+function removeDuplicates(s, k){
+    
+}
