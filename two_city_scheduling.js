26446d340d8c0c944ee2dc402aafb15ce770e5a9
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-07 16:24:56 -0500

    added solution

--- two_city_scheduling.js
+++ two_city_scheduling.js
@@ -13,7 +13,7 @@
 
 // The total minimum cost is 10 + 30 + 50 + 20 = 110 to have half the people interviewing in each city.
 
-
+// this doesn't account for dividing people equally
 function twoCityScheduling(costs){
     let total = 0
 
@@ -21,5 +21,47 @@ function twoCityScheduling(costs){
         person[0] >= person[1] ? total += person[1] : total += person[0]
     }
 
+    return total
+}
+
+function twoCityScheduling(costs){
+    // sort people by cost
+    // set a cap to have cost / 2 to divide people equally between cities
+    // set a counter for each city
+    // can only add to a city if cap hasn't been met
+
+    costs.sort((a, b) => {
+        return Math.abs(b[0] - b[1]) - Math.abs(a[0] - a[1])
+    })
+
+    let total = 0
+    let cityA = 0
+    let cityB = 0
+    let cap = costs.length/2
+
+    for (let i = 0; i < costs.length; i++){
+        let costA = costs[i][0]
+        let costB = costs[i][1]
+
+        if (costA <= costB){
+            if (cityA < cap){
+                total += costA
+                cityA++
+            } else {
+                total += costB
+                cityB++
+            }
+        }
+
+        else {
+            if (cityB < cap){
+                total += costB
+                cityB++
+            } else {
+                total += costA
+                cityA++
+            }
+        }
+    }
     return total
 }
