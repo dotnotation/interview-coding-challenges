b41c5d37abb9b4aa0c7991d1e6b7058410929dac
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-07 15:46:48 -0500

    added other solutions

--- subset.js
+++ subset.js
@@ -19,3 +19,55 @@ function subsets(nums){
     }
     return results
 }
+
+// alternate solution from _kapi1
+var subsets = function (nums) {
+	//Array finalOutput stores all the power sets
+	let finalOutput = [];
+
+	// Helper method is a recursive function that call's itself
+	const helper = (currentIndex, set) => {
+		// Check for base case as stated above
+		if (currentIndex >= nums.length) {
+			// push the set of current iteration in finalOutput
+			finalOutput.push(set);
+			return; //Necessary to end the recursion iteration
+		}
+
+		//This is the 1st case where we are including the currentIndex number
+		// We increment currentIndex and add currentNumber i.e nums[currentIndex] in our set i.e. [...set, nums[currentIndex]]
+		helper(currentIndex + 1, [...set, nums[currentIndex]]);
+
+		// This is the second case where we are excluding the currentIndex number
+		// We increment currentIndex and our set remains unchanged
+		helper(currentIndex + 1, set);
+	};
+	//1st call to recursive function
+	helper(0, []);
+
+	// return our power set stored in finalOutput
+	return finalOutput;
+};
+
+// alternate from KaziNizamul
+var subsets = function(nums) {
+	let res=[]                    // the final arr, which we will display
+	let auxArr = [], i=0             // global vars
+    
+    function recur(nums,i,auxArr){
+        if(i==nums.length) { res.push(auxArr); return } //operation of recursion will be upto i=n-1
+													 // when it will hit, i==n, it will store the computed arr, in the final arr, and break(return)
+        
+		// take it
+        recur(nums, i+1, [...auxArr, nums[i] ] ) //or, we can use 'aux.concat(nums[i])'
+		
+		// dont take
+        recur(nums, i+1, auxArr)
+    
+    }
+    
+    recur(nums,i,auxArr) // passing the global variable declared already
+    return res        // rerturn the final 2d arr
+    
+    
+};
