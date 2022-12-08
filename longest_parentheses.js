4f168979d17e96d4aca9194f92d841bf0d8a4e8d
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-08 15:00:05 -0500

    started function and added notes

---
+++ longest_parentheses.js
@@ -0,0 +1,12 @@
+// accepts a string on parentheses
+// return the length/number of the valid parentheses
+
+function longestParentheses(s){
+    // counter for both types of parentheses
+    // the length will be the number of valid open and close combined
+    // the open and close amount must be the same number
+    let open = 0
+    let close = 0
+    let maxLength = 0
+
+}
