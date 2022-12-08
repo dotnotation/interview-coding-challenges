7a6b910fbfc17fb02d3b6732cf386e755a7c6c48
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-08 15:25:03 -0500

    passed all but one test

--- longest_parentheses.js
+++ longest_parentheses.js
@@ -5,20 +5,25 @@ function longestParentheses(s){
     // counter for both types of parentheses
     // the length will be the number of valid open and close combined
     // the open and close amount must be the same number
+    
     let open = 0
     let close = 0
     let maxLength = 0
-
+    debugger 
     for (let i =0; i < s.length; i++){
         if (s[i] === "("){
             open++
         } else if (s[i] === ")"){
             close++
-        } else if (open === close){
-            maxLength = Math.max(maxLength, open + close)
-        } else {
-            open < close ? maxLength = open * 2 : close * 2
-        }
+        } 
+    }
+
+    if (open === 1 && close === 1){
+        maxLength = 0
+    } else if (open === close){
+        maxLength = Math.max(maxLength, open + close)
+    } else {
+        open < close ? maxLength = open * 2 : maxLength = close * 2
     }
 
     return maxLength
