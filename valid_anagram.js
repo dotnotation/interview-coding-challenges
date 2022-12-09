bef8d58c7ea8a2a956b5ce4c6debc7a96d723aed
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-09 14:28:56 -0500

    added solution

--- valid_anagram.js
+++ valid_anagram.js
@@ -3,5 +3,31 @@
 // returns true or false
 
 function validAnagram(s, t){
+    // loop through s and loop of t
+    // compare beginning of s to end of t
+    // save in object? 
+    let sObj = {}
+    let tObj = {}
+    if (s.length !== t.length){
+        return false
+    }
+    for (let val of s) {
+        sObj[val] = (sObj[val] || 0) + 1
+    }
     
+    for (let val of t){
+        tObj[val] = (tObj[val] || 0) + 1
+    } 
+
+    for (let key in sObj){
+        if (!(key in tObj)){
+            return false
+        }
+        if (sObj[key] !== tObj[key]){
+            return false
+        }
+    }
+    return true
 }
+
+validAnagram("car", "cat")
