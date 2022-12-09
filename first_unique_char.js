de1dce93aab5ad40b9ce95ef4194477ba9846469
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-09 16:35:15 -0500

    added my solution

--- first_unique_char.js
+++ first_unique_char.js
@@ -5,5 +5,27 @@
 // only contains lowercase letters, no special characters
 
 function firstUniqueChar(s){
-    
+    // iterate through string
+    // store in object and return the first key whose value is 1
+        // would store key as index not letter
+    // maybe stack would be better? 
+        // could pop off if the count > 1
+    let obj = {}
+
+    if (s.length <= 1) return 0
+
+    for (let i = 0; i < s.length; i++){
+        const char = s[i]
+        obj[char] = (obj[char] || 0) + 1
+    }
+
+
+    for (let i = 0; i < s.length; i++){
+        const char = s[i]
+        if (obj[char] === 1){
+            return i
+        }
+    }
+
+    return -1
 }
