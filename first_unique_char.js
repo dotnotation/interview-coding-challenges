9a93a162400514456694c8783054f3caa4834547
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-09 14:42:08 -0500

    added prompt and base function

---
+++ first_unique_char.js
@@ -0,0 +1,9 @@
+// takes in a string (s)
+// find the first non-repeating character
+// return the index
+// if it doesn't exist, return -1
+// only contains lowercase letters, no special characters
+
+function firstUniqueChar(s){
+    
+}
