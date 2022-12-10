1ecf66e6d1523331e3aa7368a55b9253f1def8bb
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-10 15:32:40 -0500

    added notes and set variables

--- odd_even_linked_list.js
+++ odd_even_linked_list.js
@@ -9,10 +9,19 @@
 // basically all the odd indices should be returned in the same order as the appeared in the original order
 // needs to be space: 0(1) time: O(n)
 
+// as I have not used linked lists before, I followed a tutorial by Shirin Setayesh
+
 function oddEvenLinkedList(head){
-    // maybe approach it like bubble sort?
-    // if index is odd keep it in place
-    // if index is even move it forward
-    // maybe best to put even index at back of array and keep going through til all even are at back
-    
+    // create two linked lists
+    // if index is odd, append to oddList
+    // if index is even, append to evenList
+    // connect the two lists via oddList.next = evenStart.next
+    // if node is zero or one return the head
+    let oddList = new ListNode(-1)
+    let evenList = new ListNode(-1)
+    let oddStart = oddList
+    let evenStart = evenList
+    let placeValue = 1
+    let current = head
+
 }
