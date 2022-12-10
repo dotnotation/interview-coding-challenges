f331448c8e0a45dd4f1cdc0606c94748587683ea
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-10 13:57:02 -0500

    added prompt and base function

---
+++ wiggle_sort.js
@@ -0,0 +1,8 @@
+// takes in an unsorted array of numbers
+// reorder it so that num[0] <= num[1] >= num[2] <= num[3]
+// basically alternate the higher and lower values
+// ex. input: num = [3, 5, 2, 1, 6, 4] = [3, 5, 1, 6, 2, 4]
+
+function wiggleSort(num){
+
+}
