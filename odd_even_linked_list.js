6c9b95426950b1db989f097455a14a15de153f82
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-10 15:41:08 -0500

    started logic for while statement

--- odd_even_linked_list.js
+++ odd_even_linked_list.js
@@ -1,5 +1,4 @@
 // given the head of a singly linked list
-// the head is an array of numbers
 // group all nodes with odd indices together
 // followed by all the even indices
 // return the reordered list
@@ -17,11 +16,30 @@ function oddEvenLinkedList(head){
     // if index is even, append to evenList
     // connect the two lists via oddList.next = evenStart.next
     // if node is zero or one return the head
+    if (!head || !head.next) return head
+
     let oddList = new ListNode(-1)
     let evenList = new ListNode(-1)
     let oddStart = oddList
     let evenStart = evenList
     let placeValue = 1
+    // our index starts at 1 not 0 
     let current = head
+    // this will be the current value during iteration 
+
+    while (current !== null){
+        if (placeValue % 2 === 1){
+            oddList.next = current
+            oddList = oddList.next
+            // this moves oddList forward
+        } else {
+            evenList.next = current
+            evenList = evenList.next
+        }
+        // now we want to go to the next value in the linked list
+        // so we go to the next index and move our linked list element forward
+        placeValue++
+        current = current.next
+    }
 
 }
