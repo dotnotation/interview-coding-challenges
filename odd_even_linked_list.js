f91cf3e2a2afa6ae73c2f53e711519477f6c49e6
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-10 15:16:03 -0500

    added prompt and base function

---
+++ odd_even_linked_list.js
@@ -0,0 +1,14 @@
+// given the head of a singly linked list
+// the head is an array of numbers
+// group all nodes with odd indices together
+// followed by all the even indices
+// return the reordered list
+// first node is odd
+// second node is even
+// the relative order between the two groups would remain the same as the input
+// basically all the odd indices should be returned in the same order as the appeared in the original order
+// needs to be space: 0(1) time: O(n)
+
+function oddEvenLinkedList(head){
+    
+}
