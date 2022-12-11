005087fb26e8155d3a7e96c4fcd4c01d96255b35
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-11 16:57:02 -0500

    started coding and psuedocode

--- merge_k.js
+++ merge_k.js
@@ -6,13 +6,26 @@
 // Output: [1,1,2,3,4,4,5,6]
 // Explanation: The linked-lists are:
 // [
-//   1->4->5,
-//   1->3->4,
-//   2->6
+//   1->4->5, - first list
+//   1->3->4, - second list
+//   2->6 - third list
 // ]
 // merging them into one sorted list:
 // 1->1->2->3->4->4->5->6
 
 function mergeK(lists){
-    
+    // merge two lists
+    // keep merging two lists at a time until there is only one
+    if (lists.length === 0){
+        return null
+    }
+
+    while (lists.length > 1){
+        let first = lists.shift()
+        // shift remove the first item in the array and returns the removed item
+        // this will return the first linked-list
+        // do the same for the next list
+        let second = lists.shift()
+        let merged = mergeList(a, b)
+    }
 }
