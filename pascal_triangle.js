226bfe89e7d78a631b8a631f0defc509398e6eba
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-11 15:41:28 -0500

    added prompt and base function

---
+++ pascal_triangle.js
@@ -0,0 +1,9 @@
+// takes in an integer(numRows)
+// return the first numRows of Pascal's triangle
+// Pascal's triangle: each number is the sum of the two numbers directly above it
+// ex. Input: numRows = 5
+// Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+function pascalTriangle(numRows){
+    
+}
