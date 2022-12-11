9dac2c5bb1321ab73ce7e2db9b675766d2ecb7bd
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-11 16:44:17 -0500

    added prompt, notes, and base function

---
+++ merge_k.js
@@ -0,0 +1,18 @@
+// takes in an array of k linked-lists
+// each linked-list is sorted in ascending order
+// merge all linked-lists into one sorted linked-list
+// return the sorted list
+// Input: lists = [[1,4,5],[1,3,4],[2,6]]
+// Output: [1,1,2,3,4,4,5,6]
+// Explanation: The linked-lists are:
+// [
+//   1->4->5,
+//   1->3->4,
+//   2->6
+// ]
+// merging them into one sorted list:
+// 1->1->2->3->4->4->5->6
+
+function mergeK(lists){
+    
+}
