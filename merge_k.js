37c6385b897d5bf79524a9573b9c49d5ea1057b8
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-11 17:06:57 -0500

    added solution and notes

--- merge_k.js
+++ merge_k.js
@@ -13,6 +13,31 @@
 // merging them into one sorted list:
 // 1->1->2->3->4->4->5->6
 
+// followed Shirin Setayesh's tutorial
+function mergeList(a, b){
+    let dummy = new ListNode(0)
+    let current = dummy
+    
+    while (a !== null && b !== null){
+        if (a.val < b.val){
+            // sort in ascending order
+            current.next = a
+            a = a.next
+            // moving a element forward
+        } else {
+            current.next = b
+            b = b.next
+            // moving b element forward
+        }
+        current = current.next
+        // move current forward
+    }
+    // once one of the lists is null, we want to continue with the other list if one list has more values than the other
+    current.next = a || b
+
+    return dummy.next
+}
+
 function mergeK(lists){
     // merge two lists
     // keep merging two lists at a time until there is only one
@@ -27,5 +52,7 @@ function mergeK(lists){
         // do the same for the next list
         let second = lists.shift()
         let merged = mergeList(a, b)
+        lists.push(merged)
     }
+    return lists[0]
 }
