601f7e61681c865f49df735781c14167a61b4deb
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-12 16:11:51 -0500

    add prompt and base function

---
+++ longest_palindrome.js
@@ -0,0 +1,9 @@
+// takes in a string(s)
+// palindrome is the same backwards and forwards
+// find the longest substring that is a palindrome
+// can be letters and numbers
+// ex. "babab" => "bab" or "aba"
+
+function longestPalindrome(s){
+    
+}
