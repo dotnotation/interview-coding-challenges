2832b295c96f0fc9f6c398b633d38d74a5de1375
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-12 16:24:11 -0500

    added psuedocode and named variables

--- longest_palindrome.js
+++ longest_palindrome.js
@@ -5,5 +5,16 @@
 // ex. "babab" => "bab" or "aba"
 
 function longestPalindrome(s){
+    // since it could be uppercase letters, lowercase everything first
+    // need a variable of the substring to return
+    // iterate through the string 
+    // two pointer method
+    // compare left side of string to right side of string 
+    // nested loop? 
+    s = s.toLowerCase()
+
+    let result = ""
+    let left = s[0]
+    let right = s[s.length - 1]
     
 }
