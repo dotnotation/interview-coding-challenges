4657d38acda89532e25ee808412816fc1721f696
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-19 14:31:45 -0500

    adding notes and base function

---
+++ move_zeroes.js
@@ -0,0 +1,9 @@
+// takes in an array of numbers
+// move all 0's to the end 
+    // maintain the relative order of the non-zero elements
+// can NOT make a copy of the array
+// ex. [0, 1, 0, 3, 12] -> [1, 3, 12, 0, 0]
+
+function moveZeroes(nums){
+    
+}
